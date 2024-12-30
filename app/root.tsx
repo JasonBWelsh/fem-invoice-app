bc868027fbb2ab4588c0199c724976edd972433b
Jason Welsh <jasonbwelsh@gmail.com>
2024-12-30 12:04:58 -0800

    Add figma file. Bring Remix Lnks back into root route

--- app/root.tsx
+++ app/root.tsx
@@ -5,15 +5,18 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { useEffect  } from "react";
 import type { LinksFunction } from "@remix-run/node";
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  useEffect(() => console.log('DRD?'), []);
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <Links />
       </head>
       <body>
         {children}
