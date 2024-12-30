687fac2afb50a36a5286d385fb3fa0e218415f79
Jason Welsh <jasonbwelsh@gmail.com>
2024-12-30 14:03:28 -0800

    Create invoices server

---
+++ app/models/invoice.server.ts
@@ -0,0 +1,5 @@
+import type { Invoice } from "~/types/invoice";
+import invoicesData from "~/data/data.json"; 
+// TODO: make sure your TS config allows JSON imports
+
+let invoices: Invoice[] = invoicesData as Invoice[];
